cf087d5ad49ccb399fc7829648971686a0c77ca6
subham-subhasis <65282756+subham-subhasis@users.noreply.github.com>
2025-10-25 17:26:01 +0530

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,14 +2,15 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+// const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+//   auth: {
+//     storage: AsyncStorage,
+//     autoRefreshToken: true,
+//     persistSession: true,
+//     detectSessionInUrl: false,
+//   },
+// });
+s
