13700a79c087398fecb69bcfaf6e717f351a11ac
subham-subhasis <65282756+subham-subhasis@users.noreply.github.com>
2025-11-02 17:10:00 +0530

    Updated index.tsx

--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -320,8 +320,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 4,
     borderRadius: 12,
-    //borderWidth: 1,
-    backgroundColor: '#F3F4F6',
+    borderWidth: 1,
+    //backgroundColor: '#F3F4F6',
     borderColor: '#E5E7EB',
   },
   filterChipText: {
