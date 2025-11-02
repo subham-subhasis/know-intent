92b0aa34af97389e3d70e0161658d6fc53d0821f
subham-subhasis <65282756+subham-subhasis@users.noreply.github.com>
2025-11-02 17:09:41 +0530

    Updated index.tsx

--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -317,8 +317,8 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   filterChip: {
-    paddingHorizontal: 12,
-    paddingVertical: 4,
+    //paddingHorizontal: 12,
+    //paddingVertical: 4,
     borderRadius: 12,
     borderWidth: 1,
     backgroundColor: '#F3F4F6',
